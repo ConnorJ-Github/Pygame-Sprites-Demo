a1c670300e6454196f28d306f63637f984be4239
CJenkins <c.e.jenkins001@gmail.com>
2024-05-01 16:48:27 +0100

    Project Setup

---
+++ main.py
@@ -0,0 +1,30 @@
+from settings import *
+from world import World
+
+class Game:
+    def __init__(self):
+        pygame.init()
+        self.WIN = pygame.display.set_mode((WIDTH,HEIGHT))
+        pygame.display.set_caption("Sprite Demo")
+
+        self.clock = pygame.time.Clock()
+
+        self.world_screen = World()
+        
+
+
+    def run(self):
+        while True:
+            self.clock.tick(FPS)
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.QUIT()
+
+
+            self.world_screen.run()
+
+            pygame.display.update()
+
+if __name__ == '__main__':
+    game = Game()
+    game.run()
---
+++ settings.py
@@ -0,0 +1,6 @@
+import pygame
+from pygame.math import Vector2 as vector
+
+FPS = 60 #Window FPS
+
+WIDTH, HEIGHT = 400, 400 #Controls the width and height on the game window.
