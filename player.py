b34fdde835a92455fbd9eae58916b24670d90547
CJenkins <c.e.jenkins001@gmail.com>
2024-05-02 16:52:59 +0100

    Bug Fixes + Tweaking
    Sprites was invisible due to set size.
    Size was corrected within player.py line 8.
    
    Added an offset to character position to match that of the hitbox.

--- player.py
+++ player.py
@@ -5,12 +5,12 @@ class Player(pygame.sprite.Sprite):
     def __init__(self, x, y, width, height):
 
         self.sprite_sheets = SpriteLoader()
-        self.SPRITES = self.sprite_sheets.load_sprite_sheets("Characters","Archer",32,32, True) #Sprite Loading, Directory 1, Directory 2, Width, Height
+        self.SPRITES = self.sprite_sheets.load_sprite_sheets("Characters","Archer",100,100, True) #Sprite Loading, Directory 1, Directory 2, Width, Height
 
-        self.character = pygame.Rect(x,y, width,height)
+        self.character = pygame.Rect(x - 50, y-50, width, height)
 
         self.hitbox_colour = 'red'
-        self.hitbox = pygame.Rect(x,y,width,height)
+        self.hitbox = pygame.Rect(x, y , width, height)
 
         self.sprite_mask = None
 
@@ -22,6 +22,10 @@ class Player(pygame.sprite.Sprite):
         self.y_speed = 5
         self.x_speed = 5
 
+        self.moving = False
+
+        self.animation_delay = 5
+
 
     def movement(self):
         
@@ -29,22 +33,35 @@ class Player(pygame.sprite.Sprite):
         if key[pygame.K_a] == True:
             self.character.x -= self.x_speed
             self.hitbox.x -= self.x_speed
-            self.character_direction = 'right'
+            self.character_direction = 'left'
+            self.moving = True
             self.animation_frame = 0
         if key[pygame.K_d] == True:
             self.character.x += self.x_speed
             self.hitbox.x += self.x_speed
-            self.character_direction = 'left'
+            self.character_direction = 'right'
+            self.moving = True
             self.animation_frame = 0
 
-    # def animation(self, fps):
-    #     pass
-    #     # self.move(self.x_speed, self.y_speed)
+
+    def update_sprite(self):
+        sprite_sheet = 'Idle'
+        
+        if self.moving:
+            sprite_sheet = 'Run'
+
+        sprite_sheet_name = sprite_sheet + "_" + self.character_direction
+        sprites = self.SPRITES[sprite_sheet_name]
+
+        sprite_index = (self.animation_frame // self.animation_delay) % len(sprites)
+        self.sprite = sprites[sprite_index]
+        self.animation_frame += 1
+
+
 
     def load_sprite(self, display):
-        pygame.draw.rect(display, 'blue', self.character)
+        # pygame.draw.rect(display, 'blue', self.character)
         pygame.draw.rect(display, self.hitbox_colour, self.hitbox, 1)
-
 
         self.sprite = self.SPRITES["Idle_" + self.character_direction][0]
         display.blit(self.sprite,(self.character.x, self.character.y))
@@ -53,3 +70,4 @@ class Player(pygame.sprite.Sprite):
     def update(self, display):
         self.movement()
         self.load_sprite(display)
+        self.update_sprite()
